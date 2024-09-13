cca096386836afdcd84f13aca25cd66126c14452
otaviolibel <otavio.libel@escola.pr.gov.br>
2024-09-13 11:17:09 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,77 @@
+let xBolinha = 20;
+let yBolinha = 20;
+let diametro = 20;
+let raio = diametro/2;
+let velocidadeXBolinha = 2;
+let velocidadeYBolinha = 2;
+let xRaquete = 5;
+let yRaquete = 150;
+let raqueteComprimento = 10;
+let raqueteAltura = 90;
+let colidiu = false;
+//sons do jogo
+let raquetada;
+let ponto;
+let trilha;
+function preload(){
+trilha = loadSound("trilha.mp3");
+  loadSound("raquetada.mp3");
+  ponto = loadSound("ponto.mp3");
+  }
+function setup() {
+trilha.loop();
+  createCanvas(600, 400);
+}
+
+function draw() {
+  background("hsla(0,0%,0%,0.57)");
+  mostraBolinha();
+  movimentaBolinha();
+  verificaColisaoBorda();
+  mostraRaquete();
+  movimentaRaquete();
+  verificaColisaoRaquete(xRaquete,yRaquete);
+}
+
+function mostraBolinha(){
+  circle(xBolinha,yBolinha,diametro);
+ 
+}function movimentaBolinha(){
+ 
+  xBolinha = xBolinha + velocidadeXBolinha;
+ 
+   yBolinha = yBolinha + velocidadeYBolinha;
+ 
+}
+function verificaColisaoBorda(){
+    if(yBolinha + raio > height || yBolinha - raio<0){
+    velocidadeYBolinha *=-1
+  }
+  if(xBolinha + raio > width || xBolinha - raio<0){
+    velocidadeXBolinha *=-1
+    ponto.play();
+  }
+}
+function mostraRaquete(){
+  rect(xRaquete,yRaquete,raqueteComprimento,raqueteAltura);
+}
+function movimentaRaquete(){
+  if(keyIsDown(87)){//87 é W
+    yRaquete -=5;
+  }
+  if(keyIsDown(83)){//83 é S
+    yRaquete +=5;
+  }  
+}
+function verificaColisaoRaquete(x,y){
+  colidiu = collideRectCircle(x,y,raqueteComprimento,raqueteAltura,xBolinha,yBolinha,raio);
+  if(colidiu){
+    raquetada.play();
+    velocidadeXBolinha *=-1;
+    if(xBolinha *=-1)
+    if(xBolinha < 50)
+      xBolinha = 23;
+    else
+      xBolinha = 577;
+  }
+}
